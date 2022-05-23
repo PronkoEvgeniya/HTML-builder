7d238eb1bfd4d2f00440b5cc9d33d98ded0ec94d
PronkoEvgeniya <Shreidereva@gmail.com>
2022-05-23 11:25:29 +0300

    feat: complite 04-copy-directory task (Mon, May 23, 2022 11:25:29 AM)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,20 @@
+const fsPromises = require('fs/promises');
+const path = require('path');
+const pathDirTo = path.join(__dirname, 'files-copy');
+const pathDirFrom = path.join(__dirname, 'files');
 
+async function copy() {
+    await fsPromises.rm(pathDirTo, { force: true, recursive: true });
+    await fsPromises.mkdir(pathDirTo, { recursive: true });
+    const files = await fsPromises.readdir(pathDirFrom, {withFileTypes: true});
+    for (const file of files) {
+        const from = path.join(pathDirFrom, file.name);
+        const to = path.join(pathDirTo, file.name);
+        if (file.isFile()) {
+            await fsPromises.copyFile(from, to);
+        } else {
+            await fsPromises.mkdir(to, { recursive: true });
+        }
+    }
+}
+copy()
